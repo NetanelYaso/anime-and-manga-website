af9f9c0be372103c3d0951250d854a816bcc298d
NetanelYaso <netanelyaso12@gmail.com>
2022-07-27 22:30:59 +0300

    commit-1

---
+++ js/users.js
@@ -0,0 +1,12 @@
+//  const USERS_API = "https://my-json-server.typicode.com/Jeck99/fake-server/users";
+
+// async function getUsers() {
+//     try {
+//         return await fetch(USERS_API)
+//             .then(res => res.json())
+//     }
+//     catch (error) {
+//         console.log("error");
+//     }
+//     finally { }
+// }
